b1a72490228c2499b946e160a2af8448bdee41cb
Danner, Jonas <Danner@stud.uni-heidelberg.de>
2021-08-03 17:37:22 +0200

    Edited commenting and docstrings

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A Random Walk Simulation """
+"""A Random Walk Simulation For Multiple Walkers"""
 
 
 # Python code for 2D random walk.
@@ -12,14 +12,17 @@ import matplotlib.pyplot as plt
 
 def create_walkers(number_of_steps, number_of_walkers):
     """
-    Creates a list of numpy arrays which each hold the coordinates of a walker. The lenght
-    of the list is defined by the number of walkers to be specified
-    :param number_of_steps:
-    :param number_of_walkers:
-    :return:
+    Creates a list of numpy arrays which each hold the coordinates of a walker. The coordinates
+    are randomly chosen in a range from 0 to the number of steps
+    :param number_of_steps: the number of steps the walker will make. This defines the
+    dimension of the array which represents the walker
+    :param number_of_walkers: the number of walkers defines the number of arrays, which
+    will be created
+    :return: a list, the 'scene' which contains all walkers, so all arrays containing coordinates
     """
     scene = []
     for _ in range(number_of_walkers):
+        # create the array of a walker and add it to the scene-list
         walker = np.random.randint(low=0, high=number_of_steps,
                                     size=(number_of_steps, 2))
         scene.append(walker)
@@ -27,8 +30,13 @@ def create_walkers(number_of_steps, number_of_walkers):
     return scene
 
 
-def do_the_steps(walker, number_of_steps):
-    """the main program"""
+def calculate_the_path(walker, number_of_steps):
+    """
+    Calculates the path the walker walks
+    :param walker: The array representing the walker, holding random coordinates
+    :param number_of_steps: the number of steps the walker takes
+    :return: the updated array representing the walker, holding all coordinates the walker walks to
+    """
     # split coordinates
     x_coord = walker[:, 0]
     y_coord = walker[:, 1]
@@ -55,18 +63,22 @@ def do_the_steps(walker, number_of_steps):
 
 def plot_the_paths(list_of_walkers, outputfilename):
     """
-
-    :param list_of_walkers:
-    :param outputfilename:
-    :return:
+    Creates the plot of the calculated path of the walker
+    :param list_of_walkers: Arrays representing the walkers. Each array holds the
+    coordinates of the respective path the walker walked.
+    :param outputfilename: The file which will be created with the plotted paths
     """
+    # define the number of rows and columns of subplots
     if len(list_of_walkers) == 1:
         columns_of_plots = 1
     else:
         columns_of_plots = 2
     rows_of_plots = round(len(list_of_walkers)/2)
+
+    # create the subplots
     figure, axis = plt.subplots(rows_of_plots, columns_of_plots)
     for counter in enumerate(list_of_walkers):
+        # get the coordinates of this walker, get its subplot and plot the path in it
         x_coords = counter[1][:, 0]
         y_coords = counter[1][:, 1]
 
@@ -80,20 +92,21 @@ def plot_the_paths(list_of_walkers, outputfilename):
         axis[row, column].set_title(f'Walker {counter[0]+1}')
 
     if len(list_of_walkers) % 2 != 0:
+        # if the number of walkers is odd, delete the last (unused) subplot
         figure.delaxes(axis[(rows_of_plots - 1), 1])
 
-
+    # arange subplot to not interfere each other, save the figure to the outputfile and
+    # show the plot to the user
     figure.tight_layout()
     plt.savefig(outputfilename)
     plt.show()
 
 
 def main():
-    """
-
-    :return:
-    """
+    """The main program. Reads in the user definitions, creates the scene, calculates
+    the path and plots it"""
     try:
+        # read in specifications defined by the user
         number_of_steps = int(sys.argv[1]) + 1
         number_of_walkers = int(sys.argv[2])
         outputfilename = sys.argv[3]
@@ -114,7 +127,7 @@ def main():
 
     scene = create_walkers(number_of_steps, number_of_walkers)
     for walker in scene:
-        walker = do_the_steps(walker, number_of_steps)
+        walker = calculate_the_path(walker, number_of_steps)
     plot_the_paths(scene, outputfilename)
 
 
